b1574ac44a0e5164d5a96204b69c64f96317286a
Lalit <lalit@lalits-macbook-air.local>
2018-12-09 01:15:12 +0530

    Initial Commit

---
+++ jsonHandling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonHandling
+//
+//  Created by Lalit on 09/12/18.
+//  Copyright © 2018 Lalit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
